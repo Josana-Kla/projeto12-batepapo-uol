bf817d109bcaf0e09435ffb560d6fa4f14259f46
Josana-Kla <josana.0205@gmail.com>
2022-09-04 14:50:52 -0300

    feat: connecting with MongoDB

--- projeto12-batepapo-uol-api/index.js
+++ projeto12-batepapo-uol-api/index.js
@@ -1,22 +1,30 @@
 import express from 'express';
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
 import cors from 'cors';
 import joi from 'joi';
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db('buzzquizz')
+});
 
 const participantsSchema = joi.object({
     name: joi.string().alphanum().required().empty('')
 }); 
 
-const participants = [];
 
-app.post('/participants', (req, res) => {
+app.post('/participants', async (req, res) => {
     const { name } = req.body;
     const validation = participantsSchema.validate(req.body);
-    
+
     if(validation.error) {
         const error = validation.error.details.map(detail => detail.message);
 
@@ -24,14 +32,23 @@ app.post('/participants', (req, res) => {
         return;
     };
 
-    participants.push({ name, lastStatus: Date.now() });
-    console.log(participants);
+    try {
+        const response = await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
 
-    res.sendStatus(201);
+        res.sendStatus(201);
+    } catch(error) {
+        res.sendStatus(500);
+    };
+
 });
 
 app.get('/participants', (req, res) => {
-    res.status(200).send(participants);
-})
+    db.collection('participants').find().toArray().then(data => {
+        res.send(data.map(value => ({
+            ...value,
+            _id: undefined
+        })));
+    });
+});
 
 app.listen(5000);
