ea6b64a1660bd2bd25398d818fa722cb72d1b501
Josana-Kla <josana.0205@gmail.com>
2022-09-04 16:43:55 -0300

    feat: created post messages and dayjs library installed

--- projeto12-batepapo-uol-api/index.js
+++ projeto12-batepapo-uol-api/index.js
@@ -3,6 +3,8 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import joi from 'joi';
+import dayjs from "dayjs";
+
 dotenv.config();
 
 const app = express();
@@ -16,8 +18,16 @@ mongoClient.connect().then(() => {
     db = mongoClient.db('buzzquizz')
 });
 
+const currentDate = dayjs(new Date()).format('HH:mm:ss');
+
 const participantsSchema = joi.object({
     name: joi.string().alphanum().required().empty('')
+}); 
+
+const messagesSchema = joi.object({
+    to: joi.string().alphanum().required().empty(''),
+    text: joi.string().required().empty(''),
+    type: joi.string().required().empty('').valid('message', 'private_messsage')
 }); 
 
 
@@ -33,7 +43,7 @@ app.post('/participants', async (req, res) => {
     };
 
     try {
-        const response = await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
+        await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
 
         res.sendStatus(201);
     } catch(error) {
@@ -49,6 +59,33 @@ app.get('/participants', (req, res) => {
             _id: undefined
         })));
     });
+});
+
+app.post('/messages', async (req, res) => {
+    const { to, text, type } = req.body;
+    const validation = messagesSchema.validate(req.body, {abortEarly: false});
+
+    if(validation.error) {
+        const error = validation.error.details.map(detail => detail.message);
+
+        res.status(422).send(error);
+        return;
+    };
+
+    try {
+        await db.collection('messages').insertOne({
+            from: 'Jojo',
+            to,
+            text,
+            type,
+            time: currentDate
+        });
+    
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    };
+
 });
 
 app.listen(5000);
